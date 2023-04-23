0326b493f7ddb28a740d85027e6efc58b2b38ff3
umarfchy <umarfchy@gmail.com>
2023-04-23 21:52:41 +0600

    add option to select user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,73 @@
 import { type NextPage } from "next";
+import { useState } from "react";
 // import { api } from "~/utils/api";
 
+const channels = [
+  "channel-001",
+  "channel-002",
+  "channel-003",
+  "channel-004",
+  "channel-005",
+];
+
+const users = ["user-001", "user-002", "user-003", "user-004", "user-005"];
+
+const chats = [
+  {
+    id: "chat-001",
+    channel: "channel-001",
+    participants: ["user-001", "user-002"],
+  },
+  {
+    id: "chat-002",
+    channel: "channel-002",
+    participants: ["user-001", "user-002", "user-003"],
+  },
+  {
+    id: "chat-003",
+    channel: "channel-003",
+    participants: ["user-002", "user-003"],
+  },
+];
+
 const Home: NextPage = () => {
+  const [currentUser, setCurrentUser] = useState("");
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
-  return <></>;
+  return (
+    <>
+      <>
+        {currentUser ? (
+          <div>
+            <h1>
+              Welcome <strong>{currentUser}</strong>
+            </h1>
+            <button
+              className="mr-2 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+              onClick={() => setCurrentUser("")}
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1>Login as any of the following</h1>
+            <div>
+              {users.map((user) => (
+                <button
+                  className="mr-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                  key={user}
+                  onClick={() => setCurrentUser(user)}
+                >
+                  {user}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </>
+    </>
+  );
 };
 
 export default Home;
