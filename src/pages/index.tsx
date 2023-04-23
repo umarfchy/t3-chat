9cd3d5bc6432740f5eaba12af4643c7d61342f35
umarfchy <umarfchy@gmail.com>
2023-04-23 21:34:56 +0600

    add base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+// import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Home;
