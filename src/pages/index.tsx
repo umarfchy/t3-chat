d4cfedf1aaa6d2ef461017b6fe6b470cf8bece62
umarfchy <umarfchy@gmail.com>
2023-04-23 22:19:37 +0600

    add chat container (not typesafe but working )

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,89 @@
 import { type NextPage } from "next";
-import { useState } from "react";
 // import { api } from "~/utils/api";
+
+import { useState, useEffect } from "react";
+import PubNub from "pubnub";
+import { PubNubProvider, usePubNub } from "pubnub-react";
+import { env } from "~/env.mjs";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+
+const ChatContainer = () => {
+  const pubnub = new PubNub({
+    publishKey: env.NEXT_PUBLIC_PUBLISHER_KEY,
+    subscribeKey: env.NEXT_PUBLIC_SUBSCRIBER_KEY,
+    uuid: "myUniqueUUID",
+  });
+
+  return (
+    <PubNubProvider client={pubnub}>
+      <Chat />
+    </PubNubProvider>
+  );
+};
+
+const Chat = () => {
+  const pubnub = usePubNub();
+  const [channels] = useState(["awesome-channel"]);
+  const [messages, addMessage] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (event) => {
+    const message = event.message;
+    if (typeof message === "string" || message.hasOwnProperty("text")) {
+      const text = message.text || message;
+      addMessage((messages) => [...messages, text]);
+    }
+  };
+
+  const sendMessage = (message) => {
+    if (message) {
+      pubnub
+        .publish({ channel: channels[0], message })
+        .then(() => setMessage(""));
+    }
+  };
+
+  useEffect(() => {
+    pubnub.addListener({ message: handleMessage });
+    pubnub.subscribe({ channels });
+  }, [pubnub, channels]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ event });
+  };
+
+  return (
+    <div>
+      <div>
+        <div>React Chat Example</div>
+        <div>
+          {messages.map((message, index) => {
+            return <div key={`message-${index}`}>{message}</div>;
+          })}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="text-black"
+            type="text"
+            placeholder="Type your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+
+          <button
+            onClick={(e) => {
+              sendMessage(message);
+            }}
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 const channels = [
   "channel-001",
@@ -36,6 +119,7 @@ const Home: NextPage = () => {
 
   return (
     <>
+      {/* select user */}
       <>
         {currentUser ? (
           <div>
@@ -61,6 +145,26 @@ const Home: NextPage = () => {
                 >
                   {user}
                 </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </>
+
+      {/* list chats */}
+      <>
+        {currentUser && (
+          <div>
+            <h2>Chats</h2>
+            <div>
+              {chats.map((chat) => (
+                <div
+                  className="mr-2 inline-block rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                  key={chat.id}
+                >
+                  <>{chat.channel}</>
+                  <ChatContainer />
+                </div>
               ))}
             </div>
           </div>
