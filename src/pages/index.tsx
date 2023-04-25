2581d2f2401fe6df0061c996ff90ba01632ed36d
umarfchy <umarfchy@gmail.com>
2023-04-25 14:35:15 +0600

    manage selectedChat with useState

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,7 @@ const chats = [
 
 const Home: NextPage = () => {
   const [currentUser, setCurrentUser] = useState("");
+  const [selectedChat, setSelectedChat] = useState("");
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
@@ -42,7 +43,10 @@ const Home: NextPage = () => {
             </h1>
             <button
               className="mr-2 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
-              onClick={() => setCurrentUser("")}
+              onClick={() => {
+                setCurrentUser("");
+                setSelectedChat("");
+              }}
             >
               Logout
             </button>
@@ -69,20 +73,27 @@ const Home: NextPage = () => {
       <>
         {currentUser && (
           <div>
-            <h2>Chats</h2>
+            <h2 className="mt-6">Chats</h2>
             <div>
+              {/* show chat rooms */}
               {chats.map((chat) => (
-                <div
-                  className="m-2 inline-block rounded border-2 border-blue-500 px-4 py-2 hover:border-2 hover:border-blue-700"
+                <button
+                  onClick={() => setSelectedChat(chat.channel)}
+                  className="m-2 inline-block rounded border-2 border-blue-500 px-4 py-2 font-bold hover:border-2 hover:border-blue-700"
                   key={chat.id}
                 >
-                  <p className="font-bold">{chat.channel}</p>
-                  <ChatContainer userId={currentUser} channel={chat.channel} />
-                </div>
+                  {chat.channel}
+                </button>
               ))}
             </div>
           </div>
         )}
+        {/* show chat messages */}
+        <div className="mt-8 w-96">
+          {selectedChat && currentUser && (
+            <ChatContainer userId={currentUser} channel={selectedChat} />
+          )}
+        </div>
       </>
     </>
   );
