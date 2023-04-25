a80bc99c9e98cd0d71ea534a8771b2caeeffdc3a
umarfchy <umarfchy@gmail.com>
2023-04-25 22:49:49 +0600

    clear state on logout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,9 @@ const chats = [
 ];
 
 const Home: NextPage = () => {
-  const { currentUser, setCurrentUser } = useAuth();
-  const { selectedChat, setSelectedChat } = useChat();
+  const { currentUser, setCurrentUser, clearCurrentUser } = useAuth();
+  const { selectedChat, setSelectedChat, clearMessages, clearSelectedChat } =
+    useChat();
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
@@ -46,8 +47,9 @@ const Home: NextPage = () => {
             <button
               className="mr-2 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
               onClick={() => {
-                setCurrentUser("");
-                setSelectedChat("");
+                clearCurrentUser();
+                clearMessages();
+                clearSelectedChat();
               }}
             >
               Logout
