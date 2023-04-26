da1dc0922355120776618bb86a65103767d93e0b
umarfchy <umarfchy@gmail.com>
2023-04-26 08:14:23 +0600

    add users, messages, chats routes

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -3,14 +3,36 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+  users: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findMany();
   }),
+  chats: publicProcedure
+    .input(z.object({ userId: z.string().min(1) }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.chat.findMany({
+        include: {
+          participants: true,
+        },
+        where: {
+          participants: {
+            some: {
+              id: input.userId,
+            },
+          },
+        },
+      });
+    }),
+  messages: publicProcedure
+    .input(z.object({ chatId: z.string().min(1) }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.message.findMany({
+        where: {
+          chatId: input.chatId,
+        },
+      });
+    }),
+
+  // getAll: publicProcedure.query(({ ctx }) => {
+  //   return ctx.prisma.example.findMany();
+  // }),
 });
